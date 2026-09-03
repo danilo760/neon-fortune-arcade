0498cb4c2f14abe74610779fd2eced50e9e59ebc
danilo760 <62104537+danilo760@users.noreply.github.com>
2026-09-03 06:12:40 -0400

    assets: assemble Golden Tiger reference artwork

---
+++ src/assets/golden-tiger/referenceData.ts
@@ -0,0 +1,17 @@
+import part01 from "./reference-bg-01";
+import part02 from "./reference-bg-02";
+import part03 from "./reference-bg-03";
+import part04 from "./reference-bg-04";
+import part05 from "./reference-bg-05";
+import part06 from "./reference-bg-06";
+import part07 from "./reference-bg-07";
+
+export const goldenTigerReferenceBase64 = [
+  part01,
+  part02,
+  part03,
+  part04,
+  part05,
+  part06,
+  part07,
+].join("");
