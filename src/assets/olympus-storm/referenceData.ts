1a14dfba22bbb6ca78e20d2552c4c8430fd4b3f3
danilo760 <62104537+danilo760@users.noreply.github.com>
2026-09-03 06:43:57 -0400

    feat: assemble Olympus Storm reference asset

---
+++ src/assets/olympus-storm/referenceData.ts
@@ -0,0 +1,25 @@
+import part01 from "./reference-part-01";
+import part02 from "./reference-part-02";
+import part04 from "./reference-part-04";
+import part05 from "./reference-part-05";
+import part06 from "./reference-part-06";
+import part07 from "./reference-part-07";
+import part08 from "./reference-part-08";
+import part09 from "./reference-part-09";
+import part10 from "./reference-part-10";
+import part11 from "./reference-part-11";
+import part12 from "./reference-part-12";
+
+export const olympusStormReferenceBase64 = [
+  part01,
+  part02,
+  part04,
+  part05,
+  part06,
+  part07,
+  part08,
+  part09,
+  part10,
+  part11,
+  part12,
+].join("");
