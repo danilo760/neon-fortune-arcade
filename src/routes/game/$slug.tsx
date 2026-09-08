91753a8e915867c82359c2532e4cc0df269e97df
danilo760 <62104537+danilo760@users.noreply.github.com>
2026-09-08 15:15:04 -0400

    style: elevate active Olympus Storm presentation

--- src/routes/game/$slug.tsx
+++ src/routes/game/$slug.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, notFound } from "@tanstack/react-router";
 import { lazy, Suspense } from "react";
 
 import goldenTigerPresentationCss from "../../components/arcade/GoldenTigerPresentationSafe.css?url";
-import olympusStormPresentationCss from "../../components/arcade/OlympusStormPremiumPolish.css?url";
+import olympusStormPresentationCss from "../../components/arcade/OlympusStormActivePolish.css?url";
 import candyCascadePresentationCss from "../../components/arcade/CandyCascadePremium.css?url";
 import minesPresentationCss from "../../components/arcade/MinesPresentationSafe.css?url";
 import plinkoPresentationCss from "../../components/arcade/PlinkoPresentationSafe.css?url";
